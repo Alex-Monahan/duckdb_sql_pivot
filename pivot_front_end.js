262b376085c2aec840ae19689ca452f8d3cc8278
= <alexm91@vt.edu>
2021-11-12 21:21:31 -0700

    Use fetch

--- pivot_front_end.js
+++ pivot_front_end.js
@@ -1,16 +1,8 @@
 window.onload = async function() {
     console.log('Loaded!!');
 
-    // const response = await fetch('http:localhost:3000/pivot');
+    const response = await fetch('http:localhost:3000/pivot?table_name=my_table');
 
-    // console.log(response);
+    console.log(await response.json());
 
-    function reqListener () {
-        console.log(this.responseText);
-      }
-      
-      var oReq = new XMLHttpRequest();
-      oReq.addEventListener("load", reqListener);
-      oReq.open("GET", "http:localhost:3000/pivot?table_name=my_table");
-      oReq.send();
 };
